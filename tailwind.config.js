f562882e7f04d40556882e461d5d9089730e5cc4
Suhas Herle <herlesuhas@gmail.com>
2023-02-06 15:38:11 +0530

    Trying CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)'
+      },
+
+      fontFamily:{
+        Outfit: 'Outfit'
+      }
+    }
+  },
+  plugins: [],
+}
